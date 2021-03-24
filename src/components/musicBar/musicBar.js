35ab9a78f7d9d0a203ff68555f8714a710be6ae0
张佳逸 <412051457@qq.com>
2021-03-24 15:23:10 +0800

    update 播放界面

--- src/components/musicBar/musicBar.js
+++ src/components/musicBar/musicBar.js
@@ -1,13 +1,29 @@
-export default {
+const vm = {
     data() {
         return {
-            imgUrl: require("../../assets/AppLogo.png"),
-            title: "暂无音乐",
-            artist: "artist",
+            playList: [
+                {
+                    id: -1,
+                    imgUrl: require("../../assets/AppLogo.png"),
+                    title: "暂无音乐",
+                    artist: "artist",
+                    favorite: false,
+                    favoriteAble: false,
+                }
+            ],
+            index: 0,
             playedTime: 0,
-            duration: 3600,
+            duration: 0,
             playedTimeString: "00:00",
             durationString: "--:--",
+            playing: false,
+            playModeList: [
+                "icon ion-shuffle btn",
+                "icon ion-arrow-return-right btn",
+                "icon ion-navicon-round btn"
+            ],
+            playMode: 0,
+            sound: 50,
         }
     },
     methods: {
@@ -34,9 +50,55 @@ export default {
         valueChange(e) {
             this.playedTime = e;
             this.formatTime();
-        }
+        },
+        checkFavorite() {
+            const id = this.playList[this.index].id;
+            const favorite = JSON.parse(localStorage.getItem("favorite") || "[]");
+            const index = favorite.indexOf(id);
+            this.playList[this.index].favorite = index >= 0 ? true : false;
+        },
+        favorite() {
+            const song = this.playList[this.index];
+            if (song.favoriteAble !== false) {
+                const musicId = song.id;
+                let favorite = JSON.parse(localStorage.getItem("favorite") || "[]");
+                const index = favorite.indexOf(musicId);
+                if (index >= 0) {
+                    favorite.splice(index, 1);
+                    song.favorite = false;
+                } else {
+                    favorite.unshift(musicId);
+                    song.favorite = true;
+                }
+                localStorage.setItem("favorite", JSON.stringify(favorite));
+            }
+        },
+        changePlayState() {
+            if (this.playList[this.index].id != -1) {
+                this.playing = !this.playing;
+            }
+        },
+        skipPlay(direction) {
+            if (this.playList[0].id != -1) {
+                if (direction == -1) {
+                    console.log(-1);
+                }
+                else {
+                    console.log(1);
+                }
+            }
+        },
+        changePlayMode() {
+            this.playMode++;
+            if (this.playMode >= 3) {
+                this.playMode = 0;
+            }
+        },
+
     },
     created() {
         this.formatTime();
     },
-}
+};
+
+export default vm;
--- src/components/musicBar/musicBar.vue
+++ src/components/musicBar/musicBar.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="musicBar">
     <div class="info">
-      <img :src="imgUrl" alt="" class="icon" />
+      <img :src="playList[index].imgUrl" alt="" class="icon" />
       <div class="rows">
-        <span class="title">{{ title }}</span>
-        <span class="artist">{{ artist }}</span>
+        <span class="title">{{ playList[index].title }}</span>
+        <span class="artist">{{ playList[index].artist }}</span>
       </div>
     </div>
     <div class="controller">
       <div class="button">
-        <i class="icon ion-shuffle btn"></i>
-        <i class="icon ion-skip-backward btn"></i>
-        <div class="playBackground">
-            <i class="icon ion-play btn play"></i>
-        </div>
-        <i class="icon ion-skip-forward btn"></i>
-        <i class="el-icon-star-off favorite btn"></i>
+        <!-- 切换播放状态 -->
+        <i :class="playModeList[playMode]" @click="changePlayMode"></i>
+        <!-- 前一首 -->
+        <i class="icon ion-skip-backward btn" @click="skipPlay(-1)"></i>
+        <!-- 播放 / 暂停 -->
+        <i
+          :class="`icon ${playing ? `ion-pause` : `ion-play`} btn play`"
+          @click="changePlayState"
+        ></i>
+        <!-- 后一首 -->
+        <i class="icon ion-skip-forward btn" @click="skipPlay(1)"></i>
+        <!-- favorite -->
+        <i
+          :class="`${
+            playList[index].favorite ? `el-icon-star-on` : `el-icon-star-off`
+          } favorite btn`"
+          @click="favorite"
+          ref="favorite"
+        ></i>
       </div>
+      <!-- 进度条 -->
       <div class="processBox">
         <span class="playedTime">{{ playedTimeString }}</span>
         <el-slider
@@ -30,7 +43,15 @@
         <span class="duration">{{ durationString }}</span>
       </div>
     </div>
-    <div class="btn">3</div>
+    <div class="btn">
+      <!-- 音量 -->
+      <div class="sound">
+        <i class="icon ion-android-volume-down soundIcon"></i>
+        <el-slider class="soundSlider" v-model="sound"></el-slider>
+      </div>
+      <!-- list -->
+      <i class="el-icon-s-unfold"></i>
+    </div>
   </div>
 </template>
 
